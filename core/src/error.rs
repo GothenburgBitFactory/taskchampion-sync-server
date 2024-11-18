dfd74429bb016bc6f58f3a5ee5dce8dfd58c8aac
Dustin J. Mitchell <dustin@v.igoro.us>
2024-11-17 23:03:36 -0500

    update docs

--- core/src/error.rs
+++ core/src/error.rs
@@ -1,3 +1,7 @@
+/// An error from the [`crate::Server`] type.
+///
+/// This type represents only circumstances outside the realm of the protocol, and not the specific
+/// results descriebd in the protocol documentation.
 #[derive(Debug, thiserror::Error)]
 pub enum ServerError {
     /// There is no client with the given ClientId.
@@ -5,5 +9,5 @@ pub enum ServerError {
     NoSuchClient,
 
     #[error(transparent)]
-    Other(#[from] anyhow::Error), // source and Display delegate to anyhow::Error
+    Other(#[from] anyhow::Error),
 }
